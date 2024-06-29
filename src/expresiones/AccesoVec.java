76183ce0bc4740b9aeaef0e02385cf45b266a22d
K-Giron <3611696550101@ingenieria.usac.edu.gt>
2024-06-29 01:56:17 -0600

    agregando acceso a vectores

--- src/expresiones/AccesoVec.java
+++ src/expresiones/AccesoVec.java
@@ -5,6 +5,8 @@
 package expresiones;
 
 import abstracto.Instruccion;
+import excepciones.Errores;
+import java.util.LinkedList;
 import simbolo.Arbol;
 import simbolo.Tipo;
 import simbolo.tablaSimbolos;
@@ -28,7 +30,73 @@ public class AccesoVec extends Instruccion {
 
     @Override
     public Object interpretar(Arbol arbol, tablaSimbolos tabla) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println("AccesoVec");
+        //buscar el vector
+        var vector = tabla.getVariable(this.id);
+        if (vector == null) {
+            return new Errores("SEMANTICA", "Vector no existente",
+                    this.linea, this.col);
+        }
+        if (index2 == null) {
+            //acceder a la lista de valores en la posicion del index
+            var posicion1 = this.index1.interpretar(arbol, tabla);
+            if (posicion1 instanceof Errores) {
+                return posicion1;
+            }
+            //verificar que el index sea un entero
+            if (this.index1.tipo.getTipo() != tipoDato.ENTERO) {
+                return new Errores("SEMANTICO", "Posicion de arreglo no existe",
+                        this.linea, this.col);
+            }
+            //acceder a la linkedlist vector.valor
+            var lista = (LinkedList) vector.getValor();
+            //verificar que el index sea menor al tamaño de la lista
+            if ((int) posicion1 >= lista.size()) {
+                return new Errores("SEMANTICO", "Posición de arreglo fuera de rango",
+                        this.linea, this.col);
+            }
+            this.tipo.setTipo(vector.getTipo().getTipo());
+            //obtener el valor en la posicion del index
+            return lista.get((int) posicion1);
+            
+        }else{
+            
+            //acceder a la lista de valores en la posicion del index
+            var posicion1 = this.index1.interpretar(arbol, tabla);
+            var posicion2 = this.index2.interpretar(arbol, tabla);
+            if (posicion1 instanceof Errores) {
+                return posicion1;
+            }
+            if (posicion2 instanceof Errores) {
+                return posicion2;
+            }
+            //verificar que el index sea un entero
+            if (this.index1.tipo.getTipo() != tipoDato.ENTERO) {
+                return new Errores("SEMANTICO", "Posicion de arreglo no existe",
+                        this.linea, this.col);
+            }
+            if (this.index2.tipo.getTipo() != tipoDato.ENTERO) {
+                return new Errores("SEMANTICO", "Posicion de arreglo no existe",
+                        this.linea, this.col);
+            }
+            //acceder a la linkedlist de linkedlists en vector.valor
+            var lista = (LinkedList) vector.getValor();
+            //verificar que el index no sea mayor al tamaño de la lista
+            if ((int) posicion1 >= lista.size()) {
+                return new Errores("SEMANTICO", "Posición de arreglo fuera de rango",
+                        this.linea, this.col);
+            }
+            //acceder a la linkedlist en la posicion del index
+            var lista2 = (LinkedList) lista.get((int) posicion1);
+            //verificar que el index sea menor al tamaño de la lista
+            if ((int) posicion2 >= lista2.size()) {
+                return new Errores("SEMANTICO", "Posición de arreglo fuera de rango",
+                        this.linea, this.col);
+            }
+            this.tipo.setTipo(vector.getTipo().getTipo());
+            //obtener el valor en la posicion del index
+            return lista2.get((int) posicion2);
+        }
         
     }
     
